62961a0299cc3a0fbdba2581227b9a5cfeadf6bc
Catherine MacLellan <c.f.maclellan@gmail.com>
2021-01-28 09:25:23 +0000

    task manager complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'tasks', to: 'tasks#index', as: :tasks
+  post 'tasks', to: 'tasks#create'
+  # Always start with the routes about new before the one about id.
+  # if not the router will check for the route with id = new
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
